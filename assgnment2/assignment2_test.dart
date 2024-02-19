01488b238f1ee9ce25df9b3520ee0135a4320083
Maneesh Kumar <maneesh02125@gmail.com>
2024-02-19 15:12:39 +0530

    final assignment 2

---
+++ assgnment2/assignment2_test.dart
@@ -0,0 +1,98 @@
+import 'package:test/test.dart';
+import 'Functions.dart';
+import 'user.dart';
+
+void main() {
+  test('i want to test User Class', () {
+    User testUser = User(
+        userName: 'user1',
+        userAddress: 'kanpur',
+        userAge: 5,
+        userCources: ['A', 'B', 'C', 'D'],
+        userRollNumber: 10);
+
+    String result = testUser.userAddress;
+
+    expect(result, 'kanpur');
+  });
+
+  test('i want to test Delete user', () {
+    User testUser2 = User(
+        userName: 'user2',
+        userAddress: 'kanpur',
+        userAge: 5,
+        userCources: ['A', 'B', 'C', 'E'],
+        userRollNumber: 10);
+
+    deleteUser();
+  });
+
+  test('i want to test sorting of  user(on Roll no)', () {
+    User testUser3 = User(
+        userName: 'user3',
+        userAddress: 'kanpur',
+        userAge: 5,
+        userCources: ['A', 'B', 'C', 'E'],
+        userRollNumber: 10);
+
+    User testUser4 = User(
+        userName: 'user4',
+        userAddress: 'kanpur',
+        userAge: 5,
+        userCources: ['A', 'B', 'C', 'E'],
+        userRollNumber: 2);
+
+    sortonRollNumber();
+    displayUser();
+  });
+
+  test('i want to test sorting of  user(on Age)', () {
+    User testUser3 = User(
+        userName: 'user3',
+        userAddress: 'kanpur',
+        userAge: 5,
+        userCources: ['A', 'B', 'C', 'E'],
+        userRollNumber: 10);
+
+    User testUser4 = User(
+        userName: 'user4',
+        userAddress: 'kanpur',
+        userAge: 5,
+        userCources: ['A', 'B', 'C', 'E'],
+        userRollNumber: 2);
+
+    sortOnAge();
+    displayUser();
+  });
+
+  test('i want to test sorting of  user(on Address)', () {
+    User testUser3 = User(
+        userName: 'user3',
+        userAddress: 'allahabad',
+        userAge: 5,
+        userCources: ['A', 'B', 'C', 'E'],
+        userRollNumber: 10);
+
+    User testUser4 = User(
+        userName: 'user4',
+        userAddress: 'kanpur',
+        userAge: 5,
+        userCources: ['A', 'B', 'C', 'E'],
+        userRollNumber: 2);
+
+    sortOnAddress();
+    displayUser();
+  });
+
+  test('i want to test Save  user details', () {
+    User testUser3 = User(
+        userName: 'user3',
+        userAddress: 'kanpur',
+        userAge: 5,
+        userCources: ['A', 'B', 'F', 'E'],
+        userRollNumber: 10);
+
+    saveUserDetails();
+    displayUser();
+  });
+}
